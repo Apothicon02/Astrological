b0b4f7f8c0c44119816afb617b0833e34f319634
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2022-02-21 20:55:18 -0500

    Added a sound effect when glow ink sacs are applied to amethyst clusters. Fixed players not swinging their arms when applying glow ink sacs to amethyst clusters. Applying glow ink sacs to amethyst clusters now uses the ink sac.

--- src/main/java/com/Apothic0n/Inversia/core/events/CommonModEvents.java
+++ src/main/java/com/Apothic0n/Inversia/core/events/CommonModEvents.java
@@ -11,6 +11,8 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -112,9 +114,16 @@ public class CommonModEvents {
         BlockPos pPos =  event.getHitVec().getBlockPos();
         BlockState pBlock = pLevel.getBlockState(pPos);
         ItemStack pStack = event.getItemStack();
+        Player player = event.getPlayer();
         if (!pLevel.isClientSide) { //Runs stuff on the server every time a player right clicks a block
             if (pStack.getItem() == Items.GLOW_INK_SAC && pBlock.getBlock() == Blocks.AMETHYST_CLUSTER) {
                 pLevel.setBlock(pPos, InversiaBlocks.GLOWING_AMETHYST.get().withPropertiesOf(pBlock), 2);
+                float f = Mth.randomBetween(pLevel.random, 0.8F, 1.2F);
+                pLevel.playSound((Player)null, pPos, SoundEvents.DOLPHIN_EAT, SoundSource.BLOCKS, 1.0F, f);
+                player.swing(event.getHand(), true);
+                if (!player.isCreative()) {
+                    pStack.setCount(pStack.getCount() - 1);
+                }
             }
         }
     }
